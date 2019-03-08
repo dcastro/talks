bf77caf307a8ed319c8b7e2a846996755b138600
Diogo Castro <diogo.filipe.acastro@gmail.com>
2019-03-08 15:20:07 +0000

    Proper scala support: https://github.com/FormidableLabs/prism-react-renderer/issues/22

--- scala-typeclasses/theme.js
+++ scala-typeclasses/theme.js
@@ -1,6 +1,18 @@
 import { future } from 'mdx-deck/themes'
 import oceanicNext from "./Themes/oceanicNext.js"
 
+// By default, `mdx-deck` uses `react-syntax-highlighter` for code highlighting, which supports Scala out of the box:
+// https://yduman.github.io/blog/mdx-deck/
+// https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md#syntax-highlighting
+// https://github.com/conorhastings/react-syntax-highlighter/blob/master/AVAILABLE_LANGUAGES_PRISM.MD
+//
+// However, we're using code-surfer, which uses `prism-react-renderer` instead, which does not support Scala.
+// But since Prism.js itself *does* support Scala, we can hack it like this:
+// https://github.com/FormidableLabs/prism-react-renderer/issues/22
+import Prism from 'prism-react-renderer/prism';
+(typeof global !== 'undefined' ? global : window).Prism = Prism;
+require('prismjs/components/prism-scala');
+
 export default {
   ...future,
 
@@ -9,3 +21,4 @@ export default {
     showNumbers: false,
   },
 }
+
